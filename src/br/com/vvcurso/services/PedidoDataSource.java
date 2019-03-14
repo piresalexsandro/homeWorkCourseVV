e4766a648ee17cd0992b5799c377a015c89f5af3
Alexsandro Pires <allexsandro.pires@gmail.com>
2019-03-14 04:12:25 -0300

    Consulta errada

--- src/br/com/vvcurso/services/PedidoDataSource.java
+++ src/br/com/vvcurso/services/PedidoDataSource.java
@@ -18,19 +18,20 @@ public class PedidoDataSource implements Acao {
 		switch (opcao.toUpperCase()) {
 		case "C":
 			consultar();
-			System.out.println(p);
+			//System.out.println(p);
 			break;
 		case "I":
 			Console console = new Console();
-			String retorno = incluir(console.preencherDados());
+			String retorno = incluir(p = console.preencherDados());
 			System.out.println(retorno);
 			break;
 		case "E":
 			excluir();
-			System.out.println(p);
+			//System.out.println(mapPedidos);
 			break;
 		case "A":
 			alterar();
+			//System.out.println(mapPedidos);
 			break;
 		case "S":
 			sair();
@@ -58,7 +59,12 @@ public class PedidoDataSource implements Acao {
 		System.out.print("Entre com o codigo do pedido a ser consultado: ");
 		int cdPedido = sc.nextInt();
 		p.setCodigoPedido(cdPedido); 
+		System.out.println();
 		return mapPedidos.getOrDefault(p.getCodigoPedido(), null);
+		//p = (Pedido) mapPedidos.get(p.getCodigoPedido());
+		//p = (Pedido) mapPedidos.get(cdPedido);
+		//return p;
+		//return mapPedidos.containsKey(cdPedido);
 	}
 	
 	@Override
@@ -67,21 +73,33 @@ public class PedidoDataSource implements Acao {
 		int cdPedido = sc.nextInt();
 		p.setCodigoPedido(cdPedido); 
 		mapPedidos.remove(p.getCodigoPedido());
+		System.out.println("Pedido: "+ cdPedido + ", excluido com sucesso");
+		System.out.println();
 	}
 
 	@Override
 	public void alterar() {
 
+		System.out.println();
+		System.out.print("Entre com o codigo do pedido a ser alterado: ");
+		int cdPedido = sc.nextInt();
+		//p = (Pedido) mapPedidos.get(cdPedido);
+		p.setCodigoPedido(cdPedido);
+		
+		
+		
 		if (mapPedidos.containsKey(p.getCodigoPedido())) {
 			mapPedidos.put(p.getCodigoPedido(), new Object());
 		} else {
 			System.out.println("Pedido não encontrado! Deseja incluir?");
 			this.incluir(p);
 		}
+		
 	}
 
 	@Override
 	public void sair() {
+		System.out.println();
 		System.out.print("Realmente deseja sair da aplicação? ");
 		String x = sc.next();
 		desejaSair = x;
